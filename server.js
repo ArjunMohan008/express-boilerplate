5a85e21214ad043eeaf9eaa1a96a423f0aa9fd68
arjunmohan008 <mohanarjun.juner@gmail.com>
2023-06-18 13:29:29 +0530

    feat: db connect function

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 require('dotenv').config(); // Load environment variables from .env file
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Mongoose database connection
+mongoose
+    .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("DB Connected"))
+    .catch(error => console.error(`DB connection error: ${error.message}`));
+
+mongoose.connection.on("error", err => {
+    console.log(`DB connection error: ${err.message}`);
+});
 
 // Define your routes and middleware here
 app.get('/', (req, res) => {
