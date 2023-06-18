a99550ebe63ee5d018492bff2efccffdf49dcde6
arjunmohan008 <mohanarjun.juner@gmail.com>
2023-06-18 13:01:52 +0530

    feat: dot env flow created

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
+// Define your routes and middleware here
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('Hello, World!');
+});
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
